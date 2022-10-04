faf0bee35afd61087d04a0521d22bfa9e3a29887
Yogui <zhidbcn@gmail.com>
2022-10-04 09:22:58 +0200

    update assets

--- Assets/Global/Script/LookToCamera.cs
+++ Assets/Global/Script/LookToCamera.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class LookToCamera : MonoBehaviour
 {
-    public float rotateSpeed = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
