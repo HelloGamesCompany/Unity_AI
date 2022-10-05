8bad75689961373e1be173f4c45a52e4464e130b
Adria Sellares <adsesa93@gmail.com>
2022-10-05 21:05:16 +0200

    Add CameraController script complete. (could be optimized)

--- Assets/Global/Script/CameraController.cs
+++ Assets/Global/Script/CameraController.cs
@@ -8,10 +8,12 @@ public class CameraController : MonoBehaviour
 
     [SerializeField] private float cameraRotateSpeed;
     [SerializeField] private float cameraMovementSpeed;
+    [SerializeField] private float movementArea;
 
     // Update is called once per frame
     void Update()
     {
+        camera.transform.LookAt(this.transform.position);
         if (Input.GetKey(KeyCode.Q))
         {
             this.transform.Rotate(new Vector3( 0, cameraRotateSpeed, 0));
@@ -21,10 +23,33 @@ public class CameraController : MonoBehaviour
             this.transform.Rotate(new Vector3(0, -cameraRotateSpeed, 0));
         }
 
+        Vector3 localForward = (transform.position - camera.transform.position);
+        localForward.y = 0;
+        localForward.Normalize();
+        Vector3 left = Vector3.Cross(localForward, Vector3.up).normalized;
+        Vector3 lastTransform = transform.position;
+
         if (Input.GetKey(KeyCode.W))
-        {
-            Vector3 localForward = camera.gameObject.transform.worldToLocalMatrix.MultiplyVector(camera.gameObject.transform.forward);
+        { 
             this.transform.position += localForward * cameraMovementSpeed;
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position -= localForward * cameraMovementSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position -= left * cameraMovementSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position += left * cameraMovementSpeed;
+        }
+
+        if (transform.position.magnitude > movementArea)
+        {
+            transform.position = lastTransform;
+        }
+
     }
 }
